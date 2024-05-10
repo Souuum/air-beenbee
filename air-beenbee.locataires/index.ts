351c9bea8e9ec2b0a855b35728edb05ddb64cf64
soum <hugo.soum-brugevin@efrei.net>
2024-05-10 15:25:54 +0200

    initiate express default route

--- air-beenbee.locataires/index.ts
+++ air-beenbee.locataires/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import type { Request, Response } from "express";
+
+const express = require('express');
+const app = express();
+const port = 3003;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World! from air-beenbee.locataires')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
