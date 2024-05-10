a98a30c30f7023e2c764824135af72a96c989ee9
soum <hugo.soum-brugevin@efrei.net>
2024-05-10 17:06:07 +0200

    [ft] created homepage

--- air-beenbee.app/src/main.tsx
+++ air-beenbee.app/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import HomePage from './pages/HomePage.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    
+  },
+  {path: "/about",
+  element: <HomePage/>,
+}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ air-beenbee.app/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+const HomePage = ()  => {
+  
+
+  return (
+
+    <div>
+        <h1>Home Page</h1>
+    </div>
+
+  )
+}
+
+export default HomePage;
