d49ce1ad66eedd81663401b9d83c0cd1ec001d71
Gilbert Ziade <gilbert_z2001@hotmail.com>
2024-05-11 14:13:08 +0200

    fix convention

--- air-beenbee.proprietaires/routes/proprietaire.ts
+++ air-beenbee.proprietaires/routes/proprietaire.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { getProprietaire } from '../services/proprietaire.service';
 var router = express.Router();
 
-router.get('/getProprietaire/:id_proprietaires', getProprietaire);
+router.get('/:id_proprietaires', getProprietaire);
 
 export default router;
