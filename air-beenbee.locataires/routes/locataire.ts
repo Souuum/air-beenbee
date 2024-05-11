63c6038673e3a985e0738147569faf04d4bdee04
Thomas Song <thomas.song@efrei.net>
2024-05-11 14:23:41 +0200

    added update locataire

--- air-beenbee.locataires/routes/locataire.ts
+++ air-beenbee.locataires/routes/locataire.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getLocataire } from '../services/locataire.service';
+import { getLocataire, updateLocataire } from '../services/locataire.service';
 var router = express.Router();
 
 router.get('/:id_locataire', getLocataire);
+
+router.post('/updateLocataire/:id_locataire', updateLocataire);
 
 export default router;
 
