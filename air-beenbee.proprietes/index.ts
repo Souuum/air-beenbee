eeaf31cf9dbbc800d45aac78b969abb81dfe4265
soum <hugo.soum-brugevin@efrei.net>
2024-05-11 15:20:47 +0200

    chore: Add CORS middleware and update dependencies

--- air-beenbee.proprietes/index.ts
+++ air-beenbee.proprietes/index.ts
@@ -2,17 +2,14 @@ import type { Request, Response } from "express";
 import Propriete from "./models/propriete.model";
 import proprieteRouter from './routes/propriete.route';
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3001;
 
 
 
-// app.get('/', (req: Request, res: Response) => {
-//     res.send('Hello World! from air-beenbee.proprietes')
-//   })
-
-
+app.use(cors({ origin: 'http://localhost:5173'}));
 app.use(express.json());
 
 app.use('/', proprieteRouter);
@@ -20,6 +17,7 @@ app.use('/', proprieteRouter);
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
 
 const Sequelize = require('./db.connection');
 const connection = Sequelize.connection;
